d53cd54d560b52e16bc551ea764d841984f2dcf5
DanCan30 <danielecan3008@gmail.com>
2022-06-24 18:34:11 +0200

    Win conditions and comments added

--- script/main.js
+++ script/main.js
@@ -1,47 +1,56 @@
 
     // Link variables to DOM elements
 const play = document.getElementById("play-button");
+const score = document.getElementById("score-wrapper");
+const scoreCounter = document.getElementById("score");
+const minefield = document.getElementById("minefield-wrapper");
+
+    // Set a start condition
+let endGame = false;
 
     // Add onclick function on button element
 play.addEventListener("click", function() {
-
-
-    const minefield = document.getElementById("minefield-wrapper");
+    
         // Pick the value of the option to set the difficulty
     let diffLevel = document.querySelector("#difficulty-selector").value;
 
-        // Make the playing board visible
-    minefield.classList.remove("disabled");
-
-        // Create a new playing board every time the user press on the button
+        // Make the playing board visible and reset it
+    minefield.classList.remove("disabled", "bomb");
+    score.classList.remove("disabled");
+    scoreCounter.innerHTML = "";
+    endGame = false;
     minefield.innerHTML = null;
+
     
     // Depending on the difficulty choosen, set the number of box in the grid
     let boxNumber = diffSelector(diffLevel);
 
+        // Set number of bombs, sort the position randomically and the win condition
     let bombList = [];
-
-
     const totalBombNumber = 16;
-    let newBomb;
+    let howToWin = boxNumber - totalBombNumber;
+
     
     
-    
-        for ( let i = 0; i < totalBombNumber; i++) {
-    
-            newBomb = generateRandomUniqueNumber(bombList, 1, boxNumber);
-            bombList.push(newBomb);
-    
+        // Create a new bomb basing on the number of total bombs
+    for ( let i = 0; i < totalBombNumber; i++) {
+
+            // Give the bomb a random value and push it in the bombList array
+        let newBomb = generateRandomUniqueNumber(bombList, 1, boxNumber);
+        bombList.push(newBomb);
+
     }
 
-    
+        // Print in console the list of the bombs (For the cheaters)
     console.log(bombList);
 
         for ( let i = 1; i <= boxNumber; i++) {
+
                 // Create boxes and give them a dimension depending on the difficulty
             let box = createNewBox(minefield, i, diffLevel);
-                    // Give an onclick function on the boxes to make them change background color on click
-                ChangeColorOnClick(box, box.value, "active", bombList, "bomb");
+
+                // Give an onclick function on the boxes to make them change background color on click
+            ChangeColorOnClick(scoreCounter, minefield, box, box.value, "active", bombList, "bomb", howToWin);
 
             };
         
@@ -80,33 +89,37 @@ function diffSelector(diffValue) {
 }
 
 
-
-
+    // Function to generate a random number in a certain range that will never repeat
 function generateRandomUniqueNumber(blacklist, min, max) {
 
     let randomNumber;
+
+        // Number check
     let isNumValid = false;
     
+    // When the check returns false, generate a random number
     while (isNumValid === false) {
         randomNumber = Math.floor(Math.random() * (max) + min);
         
+            // If the generated number is not in the blacklist yet, change the check status
         if (!blacklist.includes(randomNumber)) {
             isNumValid = true;
         }
 
+            // Else generate another random number
     }
 
+        // Return the number
     return randomNumber;
 }
 
 
-
-
-    // Function for the creation on the boxes and for give them style
+    // Function for the creation of the boxes and for give them style
 function createNewBox(parentToAppend, index, difficulty) {
 
         // Create a div
     let newBox = document.createElement("div");
+        // Save the value of the box
     newBox.value = index;
 
         // Change the box dimension depending on the difficulty
@@ -126,41 +139,47 @@ function createNewBox(parentToAppend, index, difficulty) {
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     // Function to change the color of boxes on click
-function ChangeColorOnClick(element, elementValue, classToAdd, bombList, bombClass) {
-
-        // Create an onclick event for the boxes
-    let activeClass = element.addEventListener("click", function() {
-
-            // Add the class with background changes
-
-
-        if (bombList.includes(elementValue)) {
-            element.classList.add(bombClass)
-        } else {
-            element.classList.add(classToAdd);
+function ChangeColorOnClick(message, parent, element, elementValue, classToActive, bombList, gameOverClass, winCondition) {
+    
+    // Create an onclick event for the boxes  
+    element.addEventListener("click", function() {
+        
+            // If endgame condition is true exit from the function
+        if (endGame === true) {
+            return;
         }
 
-            // If the box has the class that change his background print it on console
-        if (element.classList.contains(classToAdd)) {
-            console.log(elementValue);
-        };
+            // If the value of the box clicked is not in the bombList array
+        if (!bombList.includes(elementValue)) {
 
-            // Return the eventListener
-        return activeClass;
+                // Add the active class that will change the background color
+            element.classList.add(classToActive);
+                // Set a counter of the active boxes as a score counter
+            let counter = document.querySelectorAll("." + classToActive).length;
+            message.innerHTML = "SCORE: " + counter;
 
+                // If the score counter reach the number of the total boxes - the number of the bombs, the user wins and endgame condition becomes true
+            if (counter === winCondition) {
+
+                    // Print the message on screen
+                message.innerHTML = "You won!"
+                endGame = true;
+            }
+
+            // Else, if the user clicks on a bomb, add the class to change the background to red and set the endGame condition to true
+        } else {
+            element.classList.add(gameOverClass);
+            endGame = true;
+            
+                // All the grid becomes red
+            parent.classList.add(gameOverClass);
+
+                // Print the message on screen
+            message.innerHTML = "You lost."
+            
+        }
+            
+        
     });
 }
